9f05fc91d01e9abd0e2300d42e1776849a5634ff
Mercy Isaac <mercyisaac435@gmail.com>
2025-05-11 15:23:38 +0100

    modified add funds in file

--- submission/03.sh
+++ submission/03.sh
@@ -1,10 +1,27 @@
-# Created a SegWit address.
+## Created a SegWit address.
+#ADDR=$(bitcoin-cli -regtest -named getnewaddress address_type=p2sh-segwit)
+##$ bitcoin-cli -regtest getnewaddress default bech32
+#
+## Add funds to the address.
+#bitcoin-cli -regtest generatetoaddress 101 $ADDR
+#
+## Return only the Address
+#echo $ADDR
+
+
+
+# Create a SegWit address (P2SH-wrapped SegWit)
 ADDR=$(bitcoin-cli -regtest -named getnewaddress address_type=p2sh-segwit)
-#$ bitcoin-cli -regtest getnewaddress default bech32
 
-# Add funds to the address.
-#bitcoin-cli -regtest sendtoaddress $ADDR 0.
-bitcoin-cli -regtest generatetoaddress 101 $ADDR
+# Generate mining rewards to a standard address first
+MINER_ADDR=$(bitcoin-cli -regtest getnewaddress)
 
-# Return only the Address
+# Mine 101 blocks to get spendable balance
+bitcoin-cli -regtest generatetoaddress 1 $MINER_ADDR
+
+# Send some funds to your SegWit address
+bitcoin-cli -regtest sendtoaddress $ADDR 0.01
+
+
+# Output the SegWit address
 echo $ADDR
