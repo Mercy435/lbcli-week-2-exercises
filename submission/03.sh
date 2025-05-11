05647d807fcd1789183bb1285650b382ccdad7fd
Isaac Mercy <61017940+Mercy435@users.noreply.github.com>
2025-05-11 14:25:30 +0100

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -3,8 +3,8 @@ ADDR=$(bitcoin-cli -regtest -named getnewaddress address_type=p2sh-segwit)
 #$ bitcoin-cli -regtest getnewaddress default bech32
 
 # Add funds to the address.
-#$ bitcoin-cli -regtest sendtoaddress $ADDR [amount]
-bitcoin-cli -regtest sendtoaddress $ADDR 0.005
+$ bitcoin-cli -regtest sendtoaddress $ADDR [amount]
+# bitcoin-cli -regtest sendtoaddress $ADDR 0.005
 
 # Return only the Address
 echo $ADDR
